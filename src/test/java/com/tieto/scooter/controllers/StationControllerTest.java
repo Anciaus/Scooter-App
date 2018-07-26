2fc35401dbf8e2b878c3276e17ba0651c63477cb
Justas Ragauskas <justas.ragauskas@tieto.com>
2018-07-26 14:48:30 +0300

    Added test for StationController and StationService

---
+++ src/test/java/com/tieto/scooter/controllers/StationControllerTest.java
@@ -0,0 +1,61 @@
+package com.tieto.scooter.controllers;
+
+import com.tieto.scooter.models.StationViewModel;
+import com.tieto.scooter.services.StationService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.tieto.scooter.utils.Dto.setup;
+import static java.util.Arrays.asList;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(StationController.class)
+public class StationControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private StationService stationService;
+
+    @Test
+    public void stations_should_return_available_station_Json() throws Exception {
+        List<StationViewModel> stations = getStations();
+
+        given(stationService.getStations()).willReturn(stations);
+
+        mvc.perform(get("/api/station/stations")
+        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].name", is(stations.get(0).name)));
+    }
+
+    private List<StationViewModel> getStations() {
+        return new ArrayList<>(asList(
+                setup(new StationViewModel(), station -> {
+                    station.name = "testName";
+                    station.parkedScooters = 2;
+                }),
+                setup(new StationViewModel(), station -> {
+                    station.name = "testName2";
+                    station.parkedScooters = 1;
+                })
+        ));
+    }
+}
---
+++ src/test/java/com/tieto/scooter/services/StationServiceTest.java
@@ -0,0 +1,35 @@
+package com.tieto.scooter.services;
+
+import com.tieto.scooter.models.StationViewModel;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+public class StationServiceTest {
+
+    @TestConfiguration
+    static class StationServiceContextConfiguration {
+
+        @Bean
+        public StationService stationService() {
+            return new StationService();
+        }
+    }
+
+    @Autowired
+    private StationService stationService;
+
+    @Test
+    public void getStations_should_return_two_stations() {
+        List<StationViewModel> actual = stationService.getStations();
+
+        Assert.assertEquals(2, actual.size());
+    }
+}

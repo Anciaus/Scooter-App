962a81490a372fa753927ad43cf4359755ec3a6b
Justas Ragauskas <justas.ragauskas@tieto.com>
2018-07-27 11:14:55 +0300

    Added property for messagingUri

--- src/main/java/com/tieto/scooter/services/MessagingService.java
+++ src/main/java/com/tieto/scooter/services/MessagingService.java
@@ -2,6 +2,7 @@ package com.tieto.scooter.services;
 
 import com.sun.deploy.net.HttpResponse;
 import com.tieto.scooter.models.MessageDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +11,9 @@ import static com.tieto.scooter.utils.Dto.setup;
 
 @Service
 public class MessagingService {
+
+    @Value("${messagingUri}")
+    private String messagingUri;
 
     private final RestTemplate restTemplate;
 
@@ -20,9 +24,6 @@ public class MessagingService {
 
     boolean sendMessage(String phoneNumber, String text) {
 
-        //TODO: Change to real uri to be used
-        String messagingUri = "";
-
         MessageDto message = setup(new MessageDto(), m ->{
             m.phoneNumber = phoneNumber;
             m.text = text;
@@ -30,10 +31,6 @@ public class MessagingService {
 
         HttpResponse response = restTemplate.postForObject(messagingUri, message, HttpResponse.class);
 
-        if (response != null) {
-            return response.getStatusCode() == 200;
-        }
-
-        return false;
+        return (response != null ? response.getStatusCode() : 0) == 200;
     }
 }
